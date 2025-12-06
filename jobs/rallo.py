c18721ca1cbf40a193a1396a83abd72d2f63a7fb
mtrash <mtrash@mtra.sh>
2025-12-06 20:00:38 +0100

    Update rallo.py
    For phun

--- jobs/rallo.py
+++ jobs/rallo.py
@@ -1,11 +1,12 @@
 import time
+import random
 
-message = ":D"
+message = [';)', ':D', '._.']
 
 def handle(t, channel, bot=None):
     while True:
         try:
-            bot.send_message(channel, f"{message}")
+            bot.send_message(channel, f"{random.choice(message)}")
         except Exception as e:
             print(f"[ERR] rallo-Job: {e}")
         time.sleep(t)
